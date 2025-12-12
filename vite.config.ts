94d6d534a278a4c84b8ea5bac0cf631d1e2f929c
s4na <appletea.umauma@gmail.com>
2025-12-12 18:04:13 +0900

    feat: Auto-detect posts without index.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'posts/*.md',
-          dest: 'posts'
-        },
-        {
-          src: 'posts/index.json',
-          dest: 'posts'
-        }
-      ]
-    })
-  ],
+  plugins: [react()],
   base: '/blog/',
 })
